57c70228815fd8a49cf4f4f43107396198c59ad1
Dalila Conti Goyeneche <dalila.conti@globant.com>
2022-09-13 11:41:05 -0300

    navigation

--- webapp/controller/Customers.controller.js
+++ webapp/controller/Customers.controller.js
@@ -1,31 +1,43 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/UIComponent"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, UIComponent) {
         "use strict";
 
         return Controller.extend("globant.customers.controller.Customers", {
 
             onInit: function () {
 
-                const  oModel = this.getOwnerComponent().getModel();
+            },
+            
+            onPress: function ( oControlEvent){
+              let _customerID = oControlEvent.getSource().getBindingContext().getObject().CustomerID;
+            
 
-                console.log("-----------------");
-                console.log(oModel);
-
-                oModel.read("/Orders", {
-                    success: function(oData) {
-                        console.log(oData);
-                    },
-                    error: function ( error ){
-                        
-                    }
-                })
-
+              UIComponent.getRouterFor(this).navTo("RouteOrders",{
+                CustomerID : _customerID
+              });
             }
+           
+
+                //const  oModel = this.getOwnerComponent().getModel();
+
+              //  console.log("-----------------");
+              // console.log(oModel);
+
+              // oModel.read("/Orders", {
+                 //  success: function(oData) {
+                      //  console.log(oData);
+                   // },
+                   // error: function ( error ){
+                        
+                  // }
+               // })
+            
 
         });
     });
---
+++ webapp/controller/Orders.controller.js
@@ -0,0 +1,43 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/UIComponent",
+    "sap/ui/model/json/JSONModel"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, UIComponent, JSONModel) {
+        "use strict";
+
+        return Controller.extend("globant.customers.controller.Customers", {
+
+            onInit: function () {
+            UIComponent.getRouterFor(this).getRoute("RouteOrders").attachPatternMatched(this._readOrders, this);
+            },
+
+            _readOrders: function( oEvent ) {
+
+            var that = this;
+
+            const oModel = this.getOwnerComponent().getModel();
+
+            let _customerID = oEvent.getParameter("arguments").CustomerID;
+
+            let _skey = oModel.createKey("/Customers", {CustomerID: _customerID} );
+            
+            oModel.read (_skey + '/Orders',{
+            success: function(response){
+            that.getView().setModel(new JSONModel (response.results), 'Orders');
+            },
+
+            error: function(error){
+
+            }
+
+          })
+        }
+
+        });
+    });
+    
+    
